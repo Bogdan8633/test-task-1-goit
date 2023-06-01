62e2d80b8654d4aa67e9294fe0e08d4da8f7dc26
Bogdan <bogdanberezka@gmail.com>
2023-06-01 11:21:51 +0300

    fix-path&alt

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,11 +3,16 @@ import styles from './card.module.css';
 const Card = () => {
   return (
     <div className={styles.card}>
-      <a href="#" className={styles.logoLink}>
-        <img className={styles.logoImg} src={require('images/LogoPng.png')} />
+      <a href="/" className={styles.logoLink}>
+        <img
+          className={styles.logoImg}
+          src={require('images/LogoPng.png')}
+          alt="logo"
+        />
       </a>
 
       <div className={styles.backgroundImg}></div>
+      <div className={styles.avatarWrapper}></div>
     </div>
   );
 };
